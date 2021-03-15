6ced6571888c216e82b5b85e94c14f2f2821e12f
The-Real-Thisas <thisas@protonmail.com>
2021-03-15 23:01:22 +0400

    Adding filess

--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,5 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
+  repository: 'https://github.com/The-Real-Thisas/igcse-chem',
   titleSuffix: ' – IGCSE Chem',
   logo: (
     <>
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="description" content="IGCSE | Chemistry" />
+      <meta name="og:description" content="IGCSE | Chemistry" />
+      <meta name="twitter:card" content="IGCSE | Chemistry" />
+      <meta name="twitter:image" content="https://igcse-chem.vercel.app/demo.png" />
+      <meta name="twitter:site:domain" content="https://igcse-chem.vercel.app" />
+      <meta name="twitter:url" content="https://igcse-chem.vercel.app" />
+      <meta name="og:title" content="IGCSE | Chemistry" />
+      <meta name="og:image" content="https://igcse-chem.vercel.app/demo.png" />
+      <meta name="apple-mobile-web-app-title" content="IGCSE | Chem" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
